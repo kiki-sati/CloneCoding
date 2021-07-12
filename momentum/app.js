385c4ec23a4dbdb294226b66eede31481a8b3790
kimrumm <83346234+kimrumm@users.noreply.github.com>
2021-07-13 05:09:53 +0900

    Update app.js

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,17 @@
 const title = document.querySelector('div.hello:first-child h1');
 
 function handleTitleClick() {
-    title.style.color = 'blue';
+  title.style.color = 'blue';
 }
 
 function handleMouseEntera() {
-    title.innerText = 'mouse is here!';
+  title.innerText = 'mouse is here!';
 }
 
 function handleMouseLeave() {
-    title.innerText = 'mouse is gone!';
+  title.innerText = 'mouse is gone!';
 }
 
-title.addEventListener('click', handleTitleClick);
+title.onclick = ('click', handleTitleClick);
 title.addEventListener('mouseenter', handleMouseEntera);
 title.addEventListener('mouseleave', handleMouseLeave);
